c208688232bdf69d3723cf4f434744bb35a8a1ea
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-11-07 13:28:25 +0500

    audiod

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -1,33 +1,7 @@
-import React, { useEffect } from 'react';
-import http from '../axios';
-import '../App.css';
-
-// IMG
-import Back from '../assets/back.svg';
-import Forward from '../assets/forward.svg';
+import React from 'react';
 
 function Likes() {
-  useEffect(() => {
-    http
-      .get('playlists/37i9dQZF1DWWY64wDtewQt')
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  return (
-    <div className='likes'>
-      <div className='contain'>
-        <div className='next flex gap-5 pt-3 '>
-          <img src={Back} alt='rasm' />
-          <img src={Forward} alt='rasm' />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Likes</div>;
 }
 
 export default Likes;
